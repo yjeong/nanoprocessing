b71c0d82502ab6f57f6adb801514fd6dce5e3c66
YongHo, Jeong <yonghojeong57@gmail.com>
2019-12-10 01:08:50 +0100

    Add files via upload

--- condor/check_progress.py
+++ condor/check_progress.py
@@ -14,7 +14,7 @@ if year == "2017":
 	#mcfilelist=['F913867E-4152-3D46-BBD4-21577739C5C7_fatjetbaby_WJetsToLNu_HT-800To1200_TuneCP5.root']
 if year == "2018":
 	outputdir="/xrootd_user/yjeong/xrootd/nanoprocessing/2018/"
-'''
+
 mcfilelist=[
 	'65BED8D0-FB9A-8F46-B01B-A41623E5A52D_fatjetbaby_SMS-T1tbs_RPV_mGluino1000_TuneCUETP8M1.root',
 	'BD16B6A8-2FD7-8848-A2E7-F85EB04B35BD_fatjetbaby_SMS-T1tbs_RPV_mGluino1100_TuneCUETP8M1.root',
@@ -60,10 +60,59 @@ mcfilelist=[
 	'9DA03B56-3C39-5148-939C-8F6181BC422D_fatjetbaby_WWW_4F_TuneCUETP8M1.root',
 	'805B8B63-030F-624A-99F7-97B7A5038E4C_fatjetbaby_WZZ_TuneCUETP8M1.root',
 	'D0D17A1C-7E0E-EE42-9E8F-0BD3A6065138_fatjetbaby_ZZZ_TuneCUETP8M1.root',
-	'B1D295EC-DEF7-1E4C-8548-A2DB8EDEBEF9_fatjetbaby_WWZ_TuneCUETP8M1.root'
-	'0240D362-1E08-E541-BDC7-623D912C2480_fatjetbaby_TTTT_TuneCUETP8M2T4.root',
+	'B1D295EC-DEF7-1E4C-8548-A2DB8EDEBEF9_fatjetbaby_WWZ_TuneCUETP8M1.root',
+	'0240D362-1E08-E541-BDC7-623D912C2480_fatjetbaby_TTTT_TuneCUETP8M2T4.root'
 ]
-'''
+
+if year == "2017":
+	mcfilelist=[
+	'118C47B8-4AEA-E246-8960-5F047B82CD77_fatjetbaby_SMS-T1tbs_RPV_mGluino1000_TuneCP2.root',
+	'CCCA38AD-4BB0-D049-822B-DBC18AB371F8_fatjetbaby_SMS-T1tbs_RPV_mGluino1100_TuneCP2.root',
+	'CEA377F4-231F-E74C-885F-1FDA8D1E8507_fatjetbaby_SMS-T1tbs_RPV_mGluino1200_TuneCP2.root',
+	'A59915C3-F33F-7F4E-BD87-A971FF02D5DB_fatjetbaby_SMS-T1tbs_RPV_mGluino1300_TuneCP2.root',
+	'11A98E94-DFF2-554E-A0E3-BE4C5F54AA1C_fatjetbaby_SMS-T1tbs_RPV_mGluino1400_TuneCP2.root',
+	'CCB656C8-6FD5-494C-96F1-A61ADB5F989D_fatjetbaby_SMS-T1tbs_RPV_mGluino1600_TuneCP2.root',
+	'B60C3DCD-A008-7A4E-BA75-B3AFD5DA614E_fatjetbaby_SMS-T1tbs_RPV_mGluino1700_TuneCP2.root',
+	'561558F9-48F4-A44B-BE8E-0D1E445760D9_fatjetbaby_SMS-T1tbs_RPV_mGluino2000_TuneCP2.root',
+	'D8C5D653-11C2-3743-BCB7-5F50A28FE1AE_fatjetbaby_SMS-T1tbs_RPV_mGluino2100_TuneCP2.root',
+	'9EEAD34E-B715-2042-8018-D782BA71012C_fatjetbaby_SMS-T1tbs_RPV_mGluino2200_TuneCP2.root',
+	'C5B17F1F-0CE2-CF46-B5F2-39C53B2E34D1_fatjetbaby_QCD_HT100to200_TuneCP5.root',
+	'9830B7CA-BB11-5641-A0C3-EB4BBFDCEE50_fatjetbaby_QCD_HT200to300_TuneCP5.root',
+	'53E4D61D-4E40-1241-80A9-ABA93C5DF063_fatjetbaby_QCD_HT300to500_TuneCP5.root',
+	'CAEB2603-BC29-E943-BEA8-F0F0321ED5C5_fatjetbaby_QCD_HT500to700_TuneCP5.root',
+	'CA82CA10-F6E4-3F4F-BEF3-310A74A61A53_fatjetbaby_QCD_HT700to1000_TuneCP5.root',
+	'79540BE0-E33E-514D-AEDC-5C617E04DA1A_fatjetbaby_QCD_HT1000to1500_TuneCP5.root',
+	'91396482-AE5F-9B4B-962F-EDA1A884DC9A_fatjetbaby_QCD_HT1500to2000_TuneCP5.root',
+	'20D60F68-810C-434A-8D5F-E875CC2B3FCA_fatjetbaby_QCD_HT2000toInf_TuneCP5.root',
+	'73C48567-85B8-2B41-805F-A9468374E071_fatjetbaby_WJetsToLNu_HT-200To400_TuneCP5.root',
+	'1BAFF7C5-BC96-0D42-B6BA-768AA6DC244A_fatjetbaby_WJetsToLNu_HT-400To600_TuneCP5.root',
+	'9ABF77A8-87C8-A743-B43D-12E65201F224_fatjetbaby_WJetsToLNu_HT-600To800_TuneCP5.root',
+	'998A45FB-35E2-CA4D-8B05-5A0CD9012981_fatjetbaby_WJetsToLNu_HT-800To1200_TuneCP5.root',
+	'912285A4-CDA6-E64C-B80F-7D71CE6EAAC8_fatjetbaby_WJetsToLNu_HT-1200To2500_TuneCP5.root',
+	'9FF4E66C-6E22-564C-9683-8C9BBCA653F9_fatjetbaby_WJetsToLNu_HT-2500ToInf_TuneCP5.root',
+	'2EB22C8F-0437-DC45-B367-D60648F680C2_fatjetbaby_ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5.root',
+	'7BE73617-2ACF-454B-9B83-8114DDD985AB_fatjetbaby_ST_t-channel_top_4f_inclusiveDecays_TuneCP5.root',
+	'2D33403A-4F83-F242-8E8B-E900C6C55662_fatjetbaby_ST_tW_top_5f_inclusiveDecays_TuneCP5.root',
+	'AC2CDECE-82D4-D941-8EBC-3E33A3E0D8E8_fatjetbaby_ST_tW_antitop_5f_inclusiveDecays_TuneCP5.root',
+	'99E72BCA-022E-0545-AA44-4642171701DF_fatjetbaby_ST_s-channel_4f_leptonDecays_TuneCP5.root',
+	'622C16AB-3234-5148-BFDF-6B35EBCBC031_fatjetbaby_DYJetsToLL_M-50_TuneCP5.root',
+	'876594AA-BA82-8543-8F3B-FC0F30FF3B86_fatjetbaby_DYJetsToLL_M-10to50_TuneCP5.root',
+	'9AD059EA-C578-3C49-908E-44A5E0CEA5A8_fatjetbaby_WZZ_TuneCP5.root',
+	'BA1440B5-8F63-8748-AD3D-6D140FDBCE34_fatjetbaby_WWZ_4F_TuneCP5.root',
+	'83D276EF-B34B-0249-847A-B2C47D98D35C_fatjetbaby_WWW_4F_TuneCP5.root',
+	'5516A1AC-5EE0-2E4A-83DC-40A7269EA0FD_fatjetbaby_ZZZ_TuneCP5.root',
+	'62AD844D-9A34-3C42-8F01-520BB88EF478_fatjetbaby_TTWJetsToLNu_TuneCP5.root',
+	'EAEC120E-D192-F042-9E3A-5EDC59F913E4_fatjetbaby_TTJets_TuneCP5.root',
+	'7CE18298-D8EB-554B-A1A9-9DA185EBB0A1_fatjetbaby_TTWJetsToQQ_TuneCP5.root',
+	'34A535F7-FAC1-604D-B226-43DB1DAB2FDD_fatjetbaby_TTZToQQ_TuneCP5.root',
+	'0D21586B-C001-6A41-A06B-63260FE54F3A_fatjetbaby_TTZToLLNuNu_M-10_TuneCP5.root',
+	'74D69851-EB7F-BF4B-AC41-17989D4FB9FF_fatjetbaby_TTWW_TuneCP5.root',
+	'8DC8188D-246C-6D41-A24A-78C0CBBC6F17_fatjetbaby_ZZ_TuneCP5.root',
+	'6483D1AD-062C-DE42-8590-3AC6BE5DF6A8_fatjetbaby_WZ_TuneCP5.root',
+	'1ABA4820-0167-4C4F-99BB-2A936554C3AD_fatjetbaby_WW_TuneCP5.root',
+	'72F9BB63-F69D-DA4D-A042-22077BE4F080_fatjetbaby_TTTT_TuneCP5.root'
+]
+
 if year == "2018":
 	mcfilelist=[
 		'226A9772-2645-E94B-A8A7-0C91FE200FBA_fatjetbaby_SMS-T1tbs_RPV_mGluino1000_TuneCP2.root',
@@ -113,25 +162,23 @@ if year == "2018":
 		'30F44A1B-E74B-6F4A-9088-1D1170542117_fatjetbaby_TTTT_TuneCP5.root'
 ]
 
-#if year == "2016" or year == "2018":
-if year == "2018":
- print('---------------------------------------------cross section check----------------------------------------------')
- print('%50s %26s' %("tag", "Xsection"))
- print('--------------------------------------------------------------------------------------------------------------')
- for i, mcname in enumerate(mcfilelist):
-  tag=mcname.replace('.root','').replace('_TuneCP5','').replace('_TuneCP2','').split("fatjetbaby_")
-  rfname = outputdir+mcname
-  f=ROOT.TFile(rfname)
-  t = f.Get("tree")
-  t.GetEntry()
-  print('%50s %26.1f' %(tag[1], t.xsec))
- print('--------------------------------------------------------------------------------------------------------------')
+print('---------------------------------------------cross section check----------------------------------------------')
+print('%46s %3s %26s' %(year, "tag", "Xsection"))
+print('--------------------------------------------------------------------------------------------------------------')
+for i, mcname in enumerate(mcfilelist):
+ tag=mcname.replace('.root','').replace('_TuneCP5','').replace('_TuneCP2','').split("fatjetbaby_")
+ rfname = outputdir+mcname
+ f=ROOT.TFile(rfname)
+ t = f.Get("tree")
+ t.GetEntry()
+ print('%50s %26.1f' %(tag[1], t.xsec))
+print('--------------------------------------------------------------------------------------------------------------')
 
 
 flistdir="/cms/ldap_home/yjeong/flist/"+year
 
 print('--------------------------------------------------------------------------------------------------------------')
-print('%50s %10s %15s %15s' %("tag", "flist", "processed", "completion"))
+print('%46s %3s %10s %15s %15s' %(year, "tag", "flist", "processed", "completion"))
 print('--------------------------------------------------------------------------------------------------------------')
 
 # compare flist and the output directory, and generate a status summary table

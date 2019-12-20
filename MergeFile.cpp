c718855478f96fc1406d5e04dbd6bd05c7899578
YongHo, Jeong <yonghojeong57@gmail.com>
2019-12-20 20:07:40 +0900

    Add files via upload

---
+++ MergeFile.cpp
@@ -0,0 +1,183 @@
+{
+	int year;
+	printf("year: ");
+	scanf("%d", &year);
+
+	if(year == 2016){
+		int NumberOfTag = 46;
+		TString tag[NumberOfTag] = {
+			"TTTT_TuneCUETP8M2T4",
+			"SMS-T1tbs_RPV_mGluino2100_TuneCUETP8M1",
+			"WJetsToLNu_HT-600To800_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino1200_TuneCUETP8M1",
+			"QCD_HT2000toInf_TuneCUETP8M1",
+			"ZZZ_TuneCUETP8M1",
+			"ST_tW_top_5f_inclusiveDecays",
+			"SMS-T1tbs_RPV_mGluino1900_TuneCUETP8M1",
+			"WZ_TuneCUETP8M1",
+			"WJetsToLNu_HT-800To1200_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino1300_TuneCUETP8M1",
+			"QCD_HT200to300_TuneCUETP8M1",
+			"ZZ_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino2000_TuneCUETP8M1",
+			"ST_t-channel_antitop_4f_inclusiveDecays",
+			"TTZToQQ_TuneCUETP8M1",
+			"ST_tW_antitop_5f_inclusiveDecays",
+			"SMS-T1tbs_RPV_mGluino1800_TuneCUETP8M1",
+			"ST_t-channel_top_4f_inclusiveDecays",
+			"SMS-T1tbs_RPV_mGluino1000_TuneCUETP8M1",
+			"QCD_HT1500to2000_TuneCUETP8M1",
+			"WJetsToLNu_HT-2500ToInf_TuneCUETP8M1",
+			"TT_TuneCUETP8M2T4",
+			"TTWJetsToQQ_TuneCUETP8M1",
+			"ST_s-channel_4f_leptonDecays",
+			"DYJetsToLL_M-50_TuneCUETP8M1",
+			"QCD_HT500to700_TuneCUETP8M1",
+			"WJetsToLNu_HT-400To600_TuneCUETP8M1",
+			"WWW_4F_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino1400_TuneCUETP8M1",
+			"WW_TuneCUETP8M1",
+			"WJetsToLNu_HT-1200To2500_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino1100_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino1600_TuneCUETP8M1",
+			"WZZ_TuneCUETP8M1",
+			"DYJetsToLL_M-10to50_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino2200_TuneCUETP8M1",
+			"QCD_HT700to1000_TuneCUETP8M1",
+			"QCD_HT300to500_TuneCUETP8M1",
+			"QCD_HT1000to1500_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino1500_TuneCUETP8M1",
+			"ST_s-channel_4f_InclusiveDecays",
+			"TTZToLLNuNu_M-10_TuneCUETP8M1",
+			"SMS-T1tbs_RPV_mGluino1700_TuneCUETP8M1",
+			"TTWJetsToLNu_TuneCUETP8M1",
+			"WWZ_TuneCUETP8M1"
+		};
+	}
+
+	if(year == 2017){
+		int NumberOfTag = 45;
+		TString tag[NumberOfTag] = {
+			"SMS-T1tbs_RPV_mGluino1600_TuneCP2",
+			"WJetsToLNu_HT-400To600_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino1700_TuneCP2",
+			"ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino1000_TuneCP2",
+			"QCD_HT1500to2000_TuneCP5",
+			"ST_t-channel_top_4f_inclusiveDecays_TuneCP5",
+			"QCD_HT300to500_TuneCP5",
+			"TTWJetsToQQ_TuneCP5",
+			"DYJetsToLL_M-50_TuneCP5",
+			"QCD_HT100to200_TuneCP5",
+			"WZZ_TuneCP5",
+			"WWZ_4F_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino2100_TuneCP2",
+			"DYJetsToLL_M-10to50_TuneCP5",
+			"WWW_4F_TuneCP5",
+			"QCD_HT2000toInf_TuneCP5",
+			"WJetsToLNu_HT-600To800_TuneCP5",
+			"ST_tW_top_5f_inclusiveDecays_TuneCP5",
+			"TTWJetsToLNu_TuneCP5",
+			"ST_tW_antitop_5f_inclusiveDecays_TuneCP5",
+			"WJetsToLNu_HT-200To400_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino1300_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino2000_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1200_TuneCP2",
+			"QCD_HT200to300_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino2200_TuneCP2",
+			"TTZToQQ_TuneCP5",
+			"TTZToLLNuNu_M-10_TuneCP5",
+			"QCD_HT1000to1500_TuneCP5",
+			"ZZ_TuneCP5",
+			"WZ_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino1400_TuneCP2",
+			"TTTT_TuneCP5",
+			"QCD_HT500to700_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino1100_TuneCP2",
+			"WW_TuneCP5",
+			"WJetsToLNu_HT-800To1200_TuneCP5",
+			"WJetsToLNu_HT-1200To2500_TuneCP5",
+			"ST_s-channel_4f_leptonDecays_TuneCP5",
+			"TTWW_TuneCP5",
+			"WJetsToLNu_HT-2500ToInf_TuneCP5",
+			"QCD_HT700to1000_TuneCP5",
+			"TTJets_TuneCP5",
+			"ZZZ_TuneCP5"
+		}
+	}
+
+	if(year == 2018){
+		int NumberOfTag = 45;
+		TString tag[NumberOfTag] = {
+			"QCD_HT1000to1500_TuneCP5",
+			"QCD_HT100to200_TuneCP5",
+			"QCD_HT1500to2000_TuneCP5",
+			"QCD_HT2000toInf_TuneCP5",
+			"QCD_HT200to300_TuneCP5",
+			"QCD_HT300to500_TuneCP5",
+			"QCD_HT500to700_TuneCP5",
+			"QCD_HT700to1000_TuneCP5",
+			"SMS-T1tbs_RPV_mGluino1000_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1100_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1200_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1300_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1400_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1500_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1600_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1700_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1800_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino1900_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino2000_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino2100_TuneCP2",
+			"SMS-T1tbs_RPV_mGluino2200_TuneCP2",
+			"ST_s-channel_4f_leptonDecays_TuneCP5",
+			"ST_t-channel_antitop_4f_InclusiveDecays_TuneCP5",
+			"ST_t-channel_top_4f_InclusiveDecays_TuneCP5",
+			"ST_tW_antitop_5f_inclusiveDecays_TuneCP5",
+			"ST_tW_top_5f_inclusiveDecays_TuneCP5",
+			"TTJets_TuneCP5",
+			"TTTT_TuneCP5",
+			"TTWJetsToLNu_TuneCP5",
+			"TTWJetsToQQ_TuneCP5",
+			"TTZToLLNuNu_M-10_TuneCP5",
+			"TTZToQQ_TuneCP5",
+			"WJetsToLNu_HT-1200To2500_TuneCP5",
+			"WJetsToLNu_HT-200To400_TuneCP5",
+			"WJetsToLNu_HT-2500ToInf_TuneCP5",
+			"WJetsToLNu_HT-400To600_TuneCP5",
+			"WJetsToLNu_HT-600To800_TuneCP5",
+			"WJetsToLNu_HT-800To1200_TuneCP5",
+			"WWW_4F_TuneCP5",
+			"WWZ_TuneCP5",
+			"WW_TuneCP5",
+			"WZZ_TuneCP5",
+			"WZ_TuneCP5",
+			"ZZZ_TuneCP5",
+			"ZZ_TuneCP5"
+		};
+	}
+
+	TString Path_sample, outputdir;
+
+	if(year == 2016){
+		Path_sample = "/xrootd_user/yjeong/xrootd/nanoprocessing/2016/";
+		outputdir = "/cms/scratch/yjeong/RPV_MC/2016/";
+	}
+	if(year == 2017){
+		Path_sample = "/xrootd_user/yjeong/xrootd/nanoprocessing/2017/";
+		outputdir = "/cms/scratch/yjeong/RPV_MC/2017/";
+	}
+	if(year == 2018){
+		Path_sample = "/xrootd_user/yjeong/xrootd/nanoprocessing/2018/";
+		outputdir = "/cms/scratch/yjeong/RPV_MC/2018/";
+	}
+
+	TChain *mc[NumberOfTag];
+	static int count;
+	for(int i = 0; i < NumberOfTag; i++ ){
+		mc[i] = new TChain("tree");
+		mc[i].Add(Path_sample+"*_fatjetbaby_"+tag[i]+".root");
+		mc[i].Merge(outputdir+tag[i]+".root");
+		cout <<"Merging process : "<<tag[i]<<" Complete !!!" <<"  "<<++count<<"/"<<NumberOfTag<<endl;
+	}
+}

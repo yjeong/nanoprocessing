179d0964fee4d9352549dab85b7b3c73e4bf40f5
YongHo, Jeong <yonghojeong57@gmail.com>
2019-12-26 18:38:22 +0900

    Add files via upload

--- MergeFile.py
+++ MergeFile.py
@@ -5,10 +5,10 @@ from ROOT import TChain, TSelector, TTree, TH1F, TCanvas, TPad, TStyle, TString
 year=sys.argv[1]
 
 simple_in = "/cms/ldap_home/jaehyeok/nanoprocessing/condor/xrootd/"
-simple_out = "/cms/scratch/yjeong/CMSSW_7_1_0/src/nanoprocessing/mc/"
+simple_out = "/cms/scratch/yjeong/CMSSW_7_1_0/src/nanoprocessing/mc_merged/"
 
 inputdir = simple_in+"2016v4/2019_12_10/skim_rpvfitnbge0/"
-outputdir = simple_out+"2016/"
+outputdir = simple_out+"2016_1/"
 flists = os.listdir(inputdir)
 
 if year == "2017":
@@ -30,7 +30,7 @@ for i in range(0,len(flists)-1):
 process = list(set(process))
 
 print('---------------------------------------------------'+year+'-------------------------------------------------------')
-print('%40s %20s %25s %20s' %("tag", "Merged files", "Not merged event", 'Merged event'))
+print('%40s %15s %20s %15s %15s' %("tag", "Merged files", "Not merged event", 'Merged event', 'Event ratio'))
 print('--------------------------------------------------------------------------------------------------------------')
 
 for flist in process:
@@ -39,9 +39,14 @@ for flist in process:
 	nfiles=mc.Add(inputdir+"*_fatjetbaby_"+flist+"_rpvfitnbge0"+".root")
 	nfiles=str(nfiles)
 	t1 = mc.GetEntries()
-	t1 = str(t1)
 	mc.Merge(outputdir+flist+"_"+nfiles+".root")
 	t2 = mc.GetEntries()
+	if t1==0 or t2==0:
+		t3 = 0
+	else:
+		t3 = t1/t2
+	t1 = str(t1)
 	t2 = str(t2)
-	print('%40s %20s %25s %20s' %(flist, nfiles, t1, t2))
+	t3 = str(t3)
+	print('%40s %15s %20s %15s %15s' %(flist, nfiles, t1, t2, t3))
 print('--------------------------------------------------------------------------------------------------------------')

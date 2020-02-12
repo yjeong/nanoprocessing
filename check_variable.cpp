2b62937a135e93f0308fb01156d177fa98a083a2
YongHo, Jeong <yonghojeong57@gmail.com>
2020-02-12 14:27:20 +0900

    Add files via upload

--- check_variable.cpp
+++ check_variable.cpp
@@ -1,35 +1,39 @@
 {
-	const int sample_num = 2;
-	//const int nBranch = 54;
-	TString inputdir[sample_num] = {"/xrootd_user/jaehyeok/xrootd/2016v4/2019_12_10/skim_rpvfitnbge0/",
-		"/xrootd_user/yjeong/xrootd/nanoprocessing/2016/"
-	};
+	const int nBranch = 2;
+	const int nSample = 2;
+	TString inputdir_1 = "/xrootd_user/jaehyeok/xrootd/2016v4/2019_12_10/skim_rpvfitnbge0/";
+	TString inputdir_2 = "/xrootd_user/yjeong/xrootd/nanoprocessing/2016/";
+
+	TString inputdir[] = {"/xrootd_user/jaehyeok/xrootd/2016v4/2019_12_10/skim_rpvfitnbge0/","/xrootd_user/yjeong/xrootd/nanoprocessing/2016/"};
+
+	TString sample_name_1 = "F25F8B84-587A-B243-B7A6-34AD053DEDC5_fatjetbaby_TT_TuneCUETP8M2T4_rpvfitnbge0";
+	TString sample_name_2 = "000F136A-D9F5-934E-8714-FB807DF9A0A6_fatjetbaby_TT_TuneCUETP8M2T4";
+
 	TString outputdir = "/cms/scratch/yjeong/CMSSW_7_1_0/src/nanoprocessing/plots/";
 
-	TString sample_name[sample_num] = {
-		"F25F8B84-587A-B243-B7A6-34AD053DEDC5_fatjetbaby_TT_TuneCUETP8M2T4_rpvfitnbge0",
-		"000F136A-D9F5-934E-8714-FB807DF9A0A6_fatjetbaby_TT_TuneCUETP8M2T4"
-	};
-	float mj12;
+	TString branch[nBranch] = {"mj12","jets_pt"};
 
-	TH1F *h1[sample_num];
-	TTree *mytree[sample_num];
-	TFile *tfile[sample_num];
-	TCanvas *c_[sample_num];
+	TH1F *h1[nBranch];
+	TH1F *h2[nBranch];
+	TTree *mytree_1;
+	TTree *mytree_2;
+	TFile *tfile_1;
+	TFile *tfile_2;
+	TCanvas *c_[nBranch];
 
-	for(int i = 0; i < sample_num; i++){
-		c_[i] = new TCanvas(Form("c_%d",i),Form(""),800,800);
-		tfile[i] = new TFile(inputdir[i]+sample_name[i]+".root");
-		h1[i] = new TH1F(Form("h1_%d",i),Form("title_%d",i),30,0,600);
-		mytree[i] = (TTree*)tfile[i]->Get("tree");
-		tfile[i]->GetObject("tree_",tree);
-		mytree[i]->SetBranchAddress("mj12",&mj12);
-		//tfile[i]->Print();
-		h1[i]->Fill(mj12);
-		c_[i]->Draw();
-		c_[i]->SaveAs(outputdir+"mj12.png");
-		/*for(int j=0; j<nBranch; j++){
-			//h1[i][j]->Fill(tree);
-		}*/
+	tfile_1 = new TFile(inputdir_1+sample_name_1+".root");
+	mytree_1 = (TTree*)tfile_1->Get("tree");
+	tfile_2 = new TFile(inputdir_2+sample_name_2+".root");
+	mytree_2 = (TTree*)tfile_2->Get("tree");
+
+	for(int j=0; j<nBranch; j++){
+		c_[j] = new TCanvas;
+		h1[j] = new TH1F(Form("h1_%d",j),branch[j],100,0,2000);
+		mytree_1->Project(Form("h1_%d",j),branch[j]);
+		h2[j] = new TH1F(Form("h2_%d",j),branch[j],100,0,2000);
+		mytree_2->Project(Form("h2_%d",j),branch[j]);
+		h1[j]->Draw();
+		h2[j]->Draw("histsame");
+		c_[j]->SaveAs(outputdir+branch[j]+".png");
 	}
 }

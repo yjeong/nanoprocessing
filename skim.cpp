17f14c6906ed82e2191c22066f97fc53aa709647
YongHo, Jeong <yonghojeong57@gmail.com>
2020-04-06 02:28:59 +0900

    Add files via upload

--- skim.cpp
+++ skim.cpp
@@ -26,7 +26,9 @@
 // * To run this code, do
 //   
 //      $root -b -q makeJetP4_notext.C++\(\"cfA_QCD_HT-250To500_TuneZ2star_8TeV-madgraph-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_UCSB2047_v71_f6_1_OWX_Test.root\",1.2,10\)
-// 
+//
+// ./skim.exe /xrootd_user/jaehyeok/xrootd/2016v4/2019_12_10/processed/ rpvfitnbge0
+//
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -60,27 +62,45 @@ const bool DEBUG = false;
 
 void skimonefile(TString inputfile, TString outputdir, TString skim) 
 {
+	bool doskim = true; 
+
   // skim cut
   TString skimcut="0";
-  if(skim=="rpvfit") skimcut="ht>1200&&nbm>0&&mj12>500&&njets>=4";
-	else if(skim=="trig") skimcut="nbm>0&&mj12>500&&njets>=4";
+  if(skim=="rpvfit") {
+		skimcut="ht>1200&&nbm>0&&mj12>500&&njets>=4";
+	}
+	else if(skim=="trig") {
+		skimcut="nbm>0&&mj12>500&&njets>=4";
+	}
+	else if(skim=="rpvfitnbge0") {
+		if(inputfile.Contains("TTJets_Tune")) skimcut="ht>1200&&mj12>500&&njets>=4&&lhe_ht<600";
+		else skimcut="ht>1200&&mj12>500&&njets>=4";
+	}
+	else if(skim=="ht1000") {
+		skimcut="ht>1000";
+	}
+	else {
+		cout << "enter a correct skim!" << endl;
+	  doskim = false; 
+	}
 
-  TChain ch("tree");
-  ch.Add(inputfile);
-  
-  TObjArray *tokens = inputfile.Tokenize("/"); 
-  TString outputfile = (dynamic_cast<TObjString*>(tokens->At(tokens->GetEntries()-1)))->GetString();
-  outputfile.ReplaceAll(".root", Form("_%s.root", skim.Data()));
+	if(doskim) {
+		TChain ch("tree");
+		ch.Add(inputfile);
 
-  TFile *newfile= new TFile(outputfile,"recreate");
-  TTree *ctree = ch.CopyTree(skimcut);
-  newfile->cd();
-  if(ctree) ctree->Write();
-  newfile->Close();
+		TObjArray *tokens = inputfile.Tokenize("/"); 
+		TString outputfile = (dynamic_cast<TObjString*>(tokens->At(tokens->GetEntries()-1)))->GetString();
+		outputfile.ReplaceAll(".root", Form("_%s.root", skim.Data()));
 
-  cout << "copying " << outputfile << " to " << outputdir << endl; 
-  gSystem->Exec(Form("mv %s %s", outputfile.Data(), outputdir.Data()));  
-  
+		TFile *newfile= new TFile(outputfile,"recreate");
+		TTree *ctree = ch.CopyTree(skimcut);
+		newfile->cd();
+		if(ctree) ctree->Write();
+		newfile->Close();
+
+		cout << "copying " << outputfile << " to " << outputdir << endl; 
+		gSystem->Exec(Form("mv %s %s", outputfile.Data(), outputdir.Data()));  
+	} 
 }
 
 
@@ -96,7 +116,7 @@ int main(int argc, char **argv)
     cout << "" << endl;
     cout << "   ./process.exe [input dir] [skim] " << endl; 
     cout << "" << endl;
-    cout << " where skim = rpvfit" << endl;
+    cout << " where skim = rpvfit, trig, rpvfitnbge0, ht1000" << endl;
     return 0;
   }
   else 
@@ -115,13 +135,18 @@ int main(int argc, char **argv)
   gSystem->mkdir(outputdir.Data());
 
   // get list of files in a directory
-  vector<TString> files = globVector(Form("%s/*.root", inputdir.Data())); 
+  //vector<TString> files = globVector(Form("%s/*.root", inputdir.Data())); 
+  //vector<TString> files = globVector(Form("%s/*_DYJets*.root", inputdir.Data())); 
+  //vector<TString> files = globVector(Form("%s/*_TT*.root", inputdir.Data())); 
+  //vector<TString> files = globVector(Form("%s/*_W*.root", inputdir.Data())); 
+  //vector<TString> files = globVector(Form("%s/*_ST*.root", inputdir.Data())); 
+  vector<TString> files = globVector(Form("%s/*TTJets_*.root", inputdir.Data())); 
 
 	cout << "skimming " << files.size() << " files" << endl;
 	
   for(int i=0; i<files.size(); i++)
   {
-    if(skim!="rpvfit" && skim!="trig") continue; 
+    //if(skim!="rpvfit" && skim!="trig") continue; 
 
     cout << "skimming: " << files.at(i) << endl; 
     skimonefile(files.at(i), outputdir, skim); 
